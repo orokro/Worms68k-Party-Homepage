8e5706246fc96984ee77ca19c608e665a6d92c41
Greg Miller <hypurban86@gmail.com>
2026-03-26 01:27:17 -0400

    Adjust bg and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const FooterWrapper = styled.footer`
 	max-width: 1200px;
 	margin: 0 auto;
 	padding: 40px 20px;
-	border-top: 1px solid var(--border);
+	/* border-top: 1px solid var(--border); */
 	text-align: center;
 	color: var(--text-dim);
 	font-size: 0.9rem;
